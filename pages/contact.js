b4741ae59908de41355fca7db9521440e10d539f
Rodrigo Waldow <rodrigo.waldow@conectt.com.br>
2019-05-09 08:47:45 -0300

    change in form, delete recaptcha

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ import Layout from '../layouts/Layout'
 import Navbar from '../components/Navbar'
 
 const ContactForm = () => (
-  <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field">
+  <form name="contact" method="POST" action="/success" data-netlify="true" netlify-honeypot="bot-field">
     <input hidden name="form-name" defaultValue="contact" />
     <div className="field is-horizontal">
       <div className="field-label is-normal">
@@ -14,7 +14,7 @@ const ContactForm = () => (
       <div className="field-body">
         <div className="field">
           <p className="control is-expanded has-icons-left">
-            <input className="input" type="text" placeholder="Name"/>
+            <input className="input" type="text" name="name" placeholder="Name"/>
             <span className="icon is-small is-left">
               <svg className="svg-inline--fa fa-user fa-w-14" aria-hidden="true" data-prefix="fas" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
             </span>
@@ -22,7 +22,7 @@ const ContactForm = () => (
         </div>
         <div className="field">
           <p className="control is-expanded has-icons-left">
-            <input className="input" type="email" placeholder="Email"/>
+            <input className="input" type="email" name="email" placeholder="Email"/>
             <span className="icon is-small is-left">
               <svg className="svg-inline--fa fa-envelope fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg>
             </span>
@@ -37,7 +37,7 @@ const ContactForm = () => (
       <div className="field-body">
         <div className="field">
           <div className="control">
-            <input className="input" type="text" placeholder="e.g. Partnership opportunity"/>
+            <input className="input" type="text" name="subject" placeholder="e.g. Partnership opportunity"/>
           </div>
         </div>
       </div>
@@ -49,7 +49,7 @@ const ContactForm = () => (
       <div className="field-body">
         <div className="field">
           <div className="control">
-            <textarea className="textarea" placeholder="Explain how we can help you"></textarea>
+            <textarea className="textarea" name="message" placeholder="Explain how we can help you"></textarea>
           </div>
         </div>
       </div>
